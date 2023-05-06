cf52ea187c001960d6940882688e399239aa564a
Anil Kumar krishanshetty <anilbms75@gmail.com>
2023-05-06 22:22:30 +0000

    working demo example

---
+++ app/(default)/anil/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: 'Home - DEMO',
+    description: 'Page description',
+  }
+  
+  export default function Home() {
+    return (
+      <>
+        <h2 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">HELLO ANIL CHANGE</h2>
+      </>
+    )
+  }
+  
